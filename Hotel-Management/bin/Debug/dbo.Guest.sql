16b255d89e6a4dca786aca267d7719f96b5da676
Marco Wolff <marcowolff17@gmail.com>
2021-07-14 00:05:28 +0200

    a

---
+++ Hotel-Management/bin/Debug/dbo.Guest.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Guest]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Lastname] NCHAR(30) NOT NULL, 
+    [Firstname] NCHAR(30) NOT NULL, 
+    [Date(arrival)] DATE NOT NULL, 
+    [Date(depature)] NCHAR(10) NOT NULL
+)
--- Hotel-Management/bin/Debug/dbo.Table.sql
+++
@@ -1,4 +0,0 @@
-﻿CREATE TABLE [dbo].[Guest]
-(
-	[Id] INT NOT NULL PRIMARY KEY
-)
