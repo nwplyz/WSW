795d662619bfcad80e2d3b5ae348c2f32c06f07e
Marco Wolff <marcowolff17@gmail.com>
2021-07-13 21:33:05 +0200

    update13.07(21:33)

---
+++ Hotel-Management/bin/Debug/dbo.BigRoom.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[BigRoom] (
+    [id]               SMALLINT   NOT NULL,
+    [RoomNumber]       SMALLINT   NOT NULL,
+    [Floor]            SMALLINT   NOT NULL,
+    [MaxPerson]        SMALLINT   NOT NULL,
+    [Size(qm)]         SMALLINT   NOT NULL,
+    [RoomClass]        SMALLINT   NULL,
+    [View]             NCHAR (25) NULL,
+    [SpecialEquipment] NCHAR (40) NULL
+);
+
